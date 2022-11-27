def918cba782d73d9043a13c625edccf836082c4
triple-v <vasilevich.account@ya.ru>
2022-11-27 18:12:37 +0300

    Работа над первым методом

--- Task/Program.cs
+++ Task/Program.cs
@@ -6,3 +6,22 @@ if (size <= 0)
 {
   Console.WriteLine($"Введено недопустимое число.");
 }
+else
+{
+  string[] array1 = new string[size];
+  void CreationArray(string[] array1)
+  {
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+      if (array1[i] != " ")
+      {
+        Console.WriteLine($"Введите {i + 1}-й элемент массива:");
+      }
+      array1[i] = Console.ReadLine()!;
+      count++;
+      Console.WriteLine();
+    }
+  }
+  CreationArray(array1);
+}
